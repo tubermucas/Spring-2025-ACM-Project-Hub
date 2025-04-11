41ad147acb651fb4d54f1026faface6295cb39c7
tubermucas <tubermucas@mail.ru>
2025-04-11 14:44:49 -0400

    add main menu and death menu

--- snake.py
+++ snake.py
@@ -4,8 +4,8 @@ import random
 import json
 
 # Initialize constants
-CELL_SIZE = 25   # Size of each grid cell in pixels
-CELL_COUNT = 30  # Number of cells horizontally and vertically (so 600x600 window)
+CELL_SIZE = 30  # Size of each grid cell in pixels
+CELL_COUNT = 20  # Number of cells horizontally and vertically (so 600x600 window)
 
 PAUSED_MENU_CELLS = 13 # Width of the paused menu
 PAUSED_MENU_HEIGHT = PAUSED_MENU_CELLS * CELL_SIZE # Height of the paused menu
@@ -22,6 +22,8 @@ SCREEN_GAME_ARENA = CELL_SIZE * CELL_COUNT # Game area size
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 200, 0) # Default snake color
+YELLOW = (255, 255, 0)
+GRAY = (150, 150, 150)
 
 DIFFICULTY_COLORS = {
     "Easy": (0, 255, 0), # Bright Green
@@ -29,34 +31,15 @@ DIFFICULTY_COLORS = {
     "Hard": (139, 0, 0) # Blood red
 }
 
-# BRIGHT_GREEN = (0, 255, 0)
-# ORANGE = (255, 165, 0)
-# BLOOD_RED = (139, 0, 0)
-# IDEA FOR BG: have 0.000000000001% chance of getting random funny bg when changing it (by Tima)
+# --- IMPORTANT VARIABLES ---
 
-def get_player_name(screen, font):
-    name = ""
-    input_active = True
-    
-    while input_active:
-        screen.fill(BLACK)
-        prompt_text = font.render("Enter your name: " + name, True, WHITE)
-        screen.blit(prompt_text, (50, SCREEN_HEIGHT // 2))
-        pygame.display.flip()
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN and name:
-                    input_active = False
-                elif event.key == pygame.K_BACKSPACE:
-                    name = name[:-1]
-                elif event.unicode.isprintable():
-                    name += event.unicode
-    
-    return name
+# Initialize Pygame & setup display
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+# Set up clock for controlling the frame rate
+clock = pygame.time.Clock()
 
 # Load background images
 backgrounds = [
@@ -76,7 +59,95 @@ SPEED_LEVELS = {"Easy": 5.0, "Normal": 10.0, "Hard": 15.0}
 SPEED_CHANGES = {"Easy": 0.25, "Normal": 0.5, "Hard": 0.75}
 speed_change = 0
 
-def show_menu(screen):
+hard_mode_unlocked = False
+
+best_score = 0
+
+'''
+IDEAS / WHAT TO DO 
+
+(In addition to main tasks like toggle function or main menu )
+
+Change bacground images: more 8-bit images that would fit the vibe 
+8-bit music (Change music to hard rockish when closer to finish the game)
+
+in pause manu change Quit to Back to MM 
+also add sound effect on/off button
+
+eastern egg in backgrounds with chance of 0.0000001% 
+
+'''
+
+
+# --- UI ---
+
+# Main menu of the game 
+def main_menu(screen):
+    global font_sellected, hard_mode_unlocked
+
+    title_font = pygame.font.Font(font_sellected, 36)
+    option_font = pygame.font.Font(font_sellected, 28)
+
+    screen.fill(BLACK)
+
+    title_x = SCREEN_WIDTH // 2 # X position of the title
+    title_y = SCREEN_HEIGHT // 8 # Y position of the title
+
+    text_x = SCREEN_WIDTH // 8 # X position of the text
+    text_y = SCREEN_HEIGHT // 8 + 100 # Y position of the text
+
+    options = ["Classic Mode", "Challenge Mode", "Quit"]
+
+    
+    selectable_indexes = [
+        i for i, option in enumerate(options) if option != 'Challenge Mode' or hard_mode_unlocked
+    ]
+
+    selected = selected_index = 0
+    selected = selectable_indexes[selected]
+
+    while True:
+        title = title_font.render("snAkeCM", True, WHITE)
+        title_rect = title.get_rect(center=(title_x, title_y))
+
+        screen.blit(title, title_rect)
+
+        for i, option in enumerate(options):
+            if option == 'Challenge Mode' and not hard_mode_unlocked:
+                color = GRAY
+            elif i == selected:
+                color = YELLOW
+            else:
+                color = WHITE
+
+            text = option_font.render(option, True, color)
+            text_rect = text.get_rect(topleft=(text_x, text_y + i * 60))
+            screen.blit(text, text_rect)
+        
+        pygame.display.flip()
+        
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    selected_index = (selected_index - 1) % len(selectable_indexes)
+                    selected = selectable_indexes[selected_index]
+                elif event.key == pygame.K_DOWN:
+                    selected_index = (selected_index + 1) % len(selectable_indexes)
+                    selected = selectable_indexes[selected_index]
+                elif event.key == pygame.K_RETURN:
+                    if options[selected] == 'Quit':
+                        pygame.quit()
+                        sys.exit()
+                    else:
+                        return options[selected]
+
+# Classic Mode screen to select the difficulty
+def classic_mode_screen(screen):
     global speed_change, font_sellected
 
     title_font = pygame.font.Font(font_sellected, 36)
@@ -118,10 +189,10 @@ def show_menu(screen):
                 elif event.key == pygame.K_DOWN:
                     selected = (selected + 1) % len(options)
                 elif event.key == pygame.K_RETURN:
-                    speed_change = SPEED_CHANGES[options[selected]]
-                    return SPEED_LEVELS[options[selected]]
-                
-def pause_menu(screen):
+                    return options[selected]
+
+# Pause Menu
+def pause_menu(screen, mode, difficulty):
     global backgrounds, current_background, font_sellected
 
     title_font = pygame.font.Font(font_sellected, 25)
@@ -137,7 +208,7 @@ def pause_menu(screen):
     text_y = y_paused + PAUSED_MENU_HEIGHT // 8 + 50 # Y position of the text
 
 
-    options = ["Resume", "Restart", "Change BG", "Music On/Off", "Exit"]
+    options = ["Resume", "Restart", "Change BG", "Music On/Off", "Back to MM"]
     selected = 0
     
     while True:
@@ -162,13 +233,15 @@ def pause_menu(screen):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     selected = (selected - 1) % len(options)
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     selected = (selected + 1) % len(options)
+                elif event.key == pygame.K_ESCAPE or event.key == pygame.K_w:
+                    return
                 elif event.key == pygame.K_RETURN:
                     if options[selected] == "Resume":
                         return
                     elif options[selected] == "Restart":
-                        main()
+                        game(mode, difficulty)
                     elif options[selected] == "Change BG":
                         current_background = (current_background + 1) % len(backgrounds)
                         screen.blit(pygame.transform.scale(backgrounds[current_background], (SCREEN_WIDTH, SCREEN_GAME_ARENA)), (0, SCOREBOARD_HEIGHT))
@@ -177,11 +250,100 @@ def pause_menu(screen):
                             pygame.mixer.music.pause()
                         else:
                             pygame.mixer.music.unpause()
-                    elif options[selected] == "Exit":
-                        pygame.quit()
-                        sys.exit()
+                    elif options[selected] == "Back to MM":
+                        main()
         
         pygame.display.flip()
+
+# death menu to show the best score and ask "Try again?"
+def death_menu(score, player_name):
+    global best_score
+
+    # update the best score
+    best_score = max(best_score, score)
+
+    # if player "loged in", save their score
+    if player_name:
+        new_entry = {'user': player_name, 'score': score}
+        # Load existing data or start with an empty list
+        try:
+            with open('user_score.json', 'r') as file:
+                scores = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            scores = []
+
+        # Append the new score
+        scores.append(new_entry)
+
+        # Save the updated list
+        with open('user_score.json', 'w') as file:
+            json.dump(scores, file, indent=4)
+            print(new_entry)
+
+    # font setting
+    title_font = pygame.font.Font(font_sellected, 25)
+    option_font = pygame.font.Font(font_sellected, 20)
+
+    # menu location
+    x_paused = (SCREEN_WIDTH - PAUSED_MENU_WIDTH) // 2 # X position of the paused menu
+    y_paused = (SCREEN_GAME_ARENA - PAUSED_MENU_HEIGHT) // 2 + SCOREBOARD_HEIGHT # Y position of the paused menu
+
+    # title position
+    title_x = x_paused + PAUSED_MENU_WIDTH // 2 # X position of the title
+    title_y = y_paused + PAUSED_MENU_HEIGHT // 8 # Y position of the title
+    
+    # text postion
+    text_x = x_paused + PAUSED_MENU_WIDTH // 8 # X position of the text
+    text_y = y_paused + PAUSED_MENU_HEIGHT // 8 + 90 # Y position of the text
+
+    options = ["Yes", "No"]
+    selected = 0
+    
+    while True:
+        pygame.draw.rect(screen, BLACK, (x_paused, y_paused, PAUSED_MENU_WIDTH, PAUSED_MENU_HEIGHT))
+        
+        # Print title
+        title = title_font.render("YOU DIED!", True, WHITE)
+        title_rect = title.get_rect(center=(title_x, title_y))
+        screen.blit(title, title_rect)
+
+        # print the best score
+        best_score_show = option_font.render(f"Best Score: {best_score}", True, WHITE)
+        score_rect = best_score_show.get_rect(center=(title_x, title_y + 40))
+        screen.blit(best_score_show, score_rect)
+
+        # print the question
+        again = option_font.render("Try again?", True, WHITE)
+        again_rect = again.get_rect(topleft=(text_x, text_y))
+        screen.blit(again, again_rect)
+
+        
+        for i, option in enumerate(options):
+            color = WHITE if i == selected else YELLOW
+
+            text = option_font.render(option, True, color)
+            text_rect = text.get_rect(topleft=(text_x, text_y + (i + 1) * 40))
+            screen.blit(text, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    selected = (selected - 1) % len(options)
+                elif event.key == pygame.K_DOWN:
+                    selected = (selected + 1) % len(options)
+                elif event.key == pygame.K_RETURN:
+                    if options[selected] == "Yes":
+                        return True
+                    elif options[selected] == "No":
+                        return False
+                    
+        pygame.display.flip()
+
+
+
 
 # --- HELPER FUNCTIONS ---
 
@@ -201,26 +363,48 @@ def get_random_food_position():
     color = get_random_color()
     return (x, y, color)
 
-# Initialize Pygame & setup display
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Snake Game")
+# Register player's name and score (Only in hard difficulty of classic mode)
+def get_player_name(screen, font):
+    name = ""
+    input_active = True
+    
+    while input_active:
+        screen.fill(BLACK)
+        prompt_text = font.render("Enter your name: " + name, True, WHITE)
+        screen.blit(prompt_text, (50, SCREEN_HEIGHT // 2))
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN and name:
+                    input_active = False
+                elif event.key == pygame.K_BACKSPACE:
+                    name = name[:-1]
+                elif event.unicode.isprintable():
+                    name += event.unicode
+    
+    return name
 
-# Set up clock for controlling the frame rate
-clock = pygame.time.Clock()
+# --- GAME ---
 
-# Show speed selection menu
-speed = show_menu(screen)
+def game(mode, difficulty):
+    global screen, current_background, best_score, hard_mode_unlocked
+    
+    # set up the speed according to the difficulty
+    speed = SPEED_LEVELS[difficulty]
+    speed_change = SPEED_CHANGES[difficulty]
 
-# Initialize sound
-pygame.mixer.init()
-pygame.mixer.music.load("Sound/Sound.mp3")  # Load background music
-pygame.mixer.music.play(-1)  # Loop the music indefinitely
-volume = 0.5
-pygame.mixer.music.set_volume(volume)
+    # Initialize sound
+    pygame.mixer.init()
+    pygame.mixer.music.load("Sound/Sound.mp3")  # Load background music
+    pygame.mixer.music.play(-1)  # Loop the music indefinitely
+    volume = 0.5
+    pygame.mixer.music.set_volume(volume)
 
-def main():
-    global current_background, volume, speed
+    
     # Initialize the snake
     # Snake is a list of (x, y) positions; start in the middle with length 3
     snake = [(CELL_COUNT // 2, CELL_COUNT // 2),
@@ -228,7 +412,12 @@ def main():
              (CELL_COUNT // 2 - 2, CELL_COUNT // 2)]
 
     font = pygame.font.Font(font_sellected, 30)
-    player_name = get_player_name(screen, font)
+
+    # Ask player's name to store it later, only in hard difficulty
+    player_name = None
+    if difficulty == 'Hard' and mode == 'Classic Mode':
+        player_name = get_player_name(screen, font)
+
     
     # Initialize snake sounds
     movement_sound = pygame.mixer.Sound("Sound/movement.wav")
@@ -242,18 +431,16 @@ def main():
     snake_color = GREEN
     food = get_random_food_position()
 
-    # Game loop
-
     # define next directions, AVOID BUG OF SNAKE GOING INTO ITSELF
     next_dx, next_dy = dx, dy
 
+    # Initialize score
     score = 0
     score_x = SCREEN_WIDTH // 17 # X position of the score
     score_y = SCOREBOARD_HEIGHT // 2 - 10  # Y position of the score
 
     # Set invisible mouse
     pygame.mouse.set_visible(False)
-    score = 0  # Initialize score
 
     running = True
     while running:
@@ -278,7 +465,7 @@ def main():
                     next_dx, next_dy = 0, 1
                     play_sound(movement_sound)
                 elif event.key == pygame.K_ESCAPE:  # Pause toggle
-                    pause_menu(screen)
+                    pause_menu(screen, mode, difficulty)
                 elif event.key == pygame.K_b:  # Change background
                     current_background = (current_background + 1) % len(backgrounds)
                 elif event.key == pygame.K_PLUS or event.key == pygame.K_EQUALS:  # Increase volume
@@ -314,27 +501,13 @@ def main():
         if new_head in snake:
             # Hit itself -> Game Over
             play_sound(death_sound)
-            pygame.time.delay(1000)  # Wait for a second to let the sound play
-            new_entry = {'user': player_name, 'score': score}
-            # Load existing data or start with an empty list
-            try:
-                with open('user_score.json', 'r') as file:
-                    scores = json.load(file)
-            except (FileNotFoundError, json.JSONDecodeError):
-                scores = []
+            pygame.time.delay(700)  # Wait for a second to let the sound play
 
-            # Append the new score
-            scores.append(new_entry)
-
-            # Save the updated list
-            with open('user_score.json', 'w') as file:
-                json.dump(scores, file, indent=4)
-            print(new_entry)
-            running = False
-
-        # Walls around the borders
-        #if (new_x < 0 or new_x >= CELL_COUNT or new_y < 0 or new_y >= CELL_COUNT) or new_head in snake:
-        #    running = False
+            # Show death menu 
+            if not death_menu(score, player_name):
+                game(mode, difficulty) # restart
+            else:
+                main() # back to mm
 
         # If still safe, insert new head
         snake.insert(0, new_head)
@@ -344,7 +517,12 @@ def main():
             play_sound(eat_sound) # play snake eat food sound
             snake_color = food[2] # changes snake color based on food
             speed += speed_change # increase snake speed after eating food
-            score += 10  # Increase score by 10 when food is eaten
+            score += 1  # Increase score by 10 when food is eaten
+
+            if mode == "Classic Mode" and not hard_mode_unlocked:
+                if (score >= 5 and difficulty == 'Hard') or (score >= 30 and difficulty == 'Normal'):
+                    hard_mode_unlocked = True
+
             # Generate a new food position; don't pop the tail (snake grows)
             temp_food = get_random_food_position()
             while temp_food in snake or temp_food is new_head:
@@ -379,6 +557,26 @@ def main():
     # Once we exit the loop, the game is over
     pygame.quit()
     sys.exit()
+
+
+def main():
+    global screen, best_score
+
+    # turn off the music 
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+
+    # show the main menu and get the mode
+    mode = main_menu(screen)
+
+    # set the difficulty that sets the speed according to the mode
+    if mode == "Challenge Mode":
+        difficulty = 'Hard'
+    else:
+        difficulty = classic_mode_screen(screen)
+
+    # run the game
+    game(mode, difficulty)
 
 if __name__ == "__main__":
     main()
